d1e4e7cccce14e2b571baa2d3f1824d07ad7e3e8
Aykrifa <aykrifa@student.42.fr>
2025-08-30 04:13:17 +0200

    clean array

--- ex03/srcs/MateriaSource.cpp
+++ ex03/srcs/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 13:51:55 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/08/30 03:47:18 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/08/30 04:12:59 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ MateriaSource::MateriaSource(void)
 
 MateriaSource::MateriaSource(MateriaSource const &copy)// copie flat
 {
+	copy_Materia_array(this->_inventory, copy._inventory, 4);
 	std::cout << "[🔧]"
 		<< "Materia Source has been created (copy) !"
 		<< std::endl;
@@ -30,6 +31,7 @@ MateriaSource::MateriaSource(MateriaSource const &copy)// copie flat
 
 MateriaSource::~MateriaSource(void) // delete
 {
+	delete_Materia_array(this->_inventory, 4);
 	std::cout << "[💥]"
 		<< "Materia Source"
 		<< " has been destroyed !"
@@ -38,17 +40,9 @@ MateriaSource::~MateriaSource(void) // delete
 
 MateriaSource			&MateriaSource::operator=(MateriaSource const &rhs)
 {
+	delete_Materia_array(this->_inventory, 4);
+	copy_Materia_array(this->_inventory, rhs._inventory, 4);
 	if (this == &rhs)
 		return (*this);
 	return (*this);
-}
-
-std::string const	&MateriaSource::getType() const
-{
-	return (this->_type);
-}
-
-void				MateriaSource::use(ICharacter& target)
-{
-	(void)target;
 }
